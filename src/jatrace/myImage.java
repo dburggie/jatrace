8709b74893c5e9f3cbc1186636df6a543e1505ad
Duncan Burggraf <dcburggraf@gmail.com>
2014-03-01 12:24:06 -0800

    myImage class documented

--- src/jatrace/myImage.java
+++ src/jatrace/myImage.java
@@ -4,6 +4,8 @@ import java.awt.image.*;
 import java.io.*;
 import javax.imageio.ImageIO;
 
+/** Provides a simplified interface to the java.awt.image, java.io, and
+ *  javax.imageio libraries. */
 public class myImage
 {
 	protected final static int ct = BufferedImage.TYPE_4BYTE_ABGR;
@@ -12,6 +14,7 @@ public class myImage
 	protected WritableRaster wr;
 	protected int width, height;
 	
+	/** Instantiates a new image with a buffer w pixels wide and h pixels tall. */
 	public myImage(int w, int h)
 	{
 		width = w; height = h;
@@ -19,12 +22,16 @@ public class myImage
 		wr = bi.getRaster();
 	}
 	
+	/** Sets pixel (x,y) to specified color. These coordinates follow the
+	 *  standard of (0,0) being the top-left of the image. */
 	public myImage setPixel(int x, int y, Color c)
 	{
 		wr.setPixel(x,y,c.p());
 		return this;
 	}
 	
+	/** Writes image buffer to given filename. Note: calls System.exit(1) on
+	 *  IOException, thus may exit program uncleanly. */
 	public myImage write(String filename)
 	{
 		try {
