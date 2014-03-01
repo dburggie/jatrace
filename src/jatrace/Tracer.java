94a7d26ba049e152f215c9dfb7195116be3c014f
Duncan Burggraf <dcburggraf@gmail.com>
2014-03-01 13:46:18 -0800

    Tracer class documented

--- src/jatrace/Tracer.java
+++ src/jatrace/Tracer.java
@@ -1,12 +1,19 @@
 package jatrace;
 
+/** This class is mostly a wrapper for the World and Camera classes and
+ *  provides methods to accomplish the actual tracing of rays and subsequent
+ *  image rendering. */
 public class Tracer
 {
+	
 	protected World world;
 	protected Camera camera;
 	protected myImage image;
 	protected int ppu, width, height;
 	
+	/** Initializes new Tracer object to given world and camera. Modification
+	 *  of the World and Camera objects after Tracer instantiation can be done
+	 *  through the normal World and Camera class methods. */
 	public Tracer(World w, Camera c)
 	{
 		this.setWorld(w);
@@ -14,6 +21,9 @@ public class Tracer
 		this.image = null;
 	}
 	
+	/** Initializes new Tracer object with default World and Camera objects.
+	 *  References to the World and Camera objects can subsequently be obtained
+	 *  through the getWorld() and getCamera() methods.*/
 	public Tracer()
 	{
 		this.setWorld();
@@ -21,22 +31,27 @@ public class Tracer
 		this.image = null;
 	}
 	
+	
+	/** Calling object will trace within a default world. */
 	public Tracer setWorld()
 	{
 		return this.setWorld(new World());
 	}
 	
+	/** Calling object will trace within the given world. */
 	public Tracer setWorld(World w)
 	{
 		this.world = w;
 		return this;
 	}
 	
+	/** Calling object will trace with default Camera settings. */
 	public Tracer setCamera()
 	{
 		return this.setCamera(new Camera());
 	}
 	
+	/** Calling object will trace with given Camera settings.*/
 	public Tracer setCamera(Camera c)
 	{
 		this.camera = c; 
@@ -46,6 +61,11 @@ public class Tracer
 		return this;
 	}
 	
+	/** Calling object will draw the its World object from the perspective of
+	 *  its Camera object. This is accomplished by tracing given number of rays
+	 *  from the camera origin through each pixel in the image. Dealiasing
+	 *  depends on the number of passes per pixel. Depth of Field blur is
+	 *  accomplished by adding a origin delta to the Camera object. */
 	public Tracer draw(int passes)
 	{
 		if (world == null)
@@ -92,17 +112,21 @@ public class Tracer
 		
 	} // END DRAW METHOD
 	
+	/** Calls draw(8). */
 	public Tracer draw()
 	{
 		return this.draw(8);
 	}
 	
+	/** Writes a drawn image with a default name to disk. */
 	public Tracer write()
 	{
 		String filename = "trace-" + width + "x" + height + ".png";
 		return this.write(filename);
 	}
 	
+	/** Writes a drawn image into a file of the given name. Note that this will
+	 *  fail fatally on an IOException. See Camera.write(). */
 	public Tracer write(String filename)
 	{
 		System.out.println("Printing to file '" + filename + "'");
